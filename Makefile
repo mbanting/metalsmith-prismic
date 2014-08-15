b5d63ea693eafc3240cfcea8a22204ff32efcb6a
Marty Banting <mban@travelunderwriters.com>
2014-08-15 16:29:23 -0700

    Added initial test

---
+++ Makefile
@@ -0,0 +1,7 @@
+node_modules: package.json
+	@npm install
+
+test: node_modules
+	@./node_modules/.bin/mocha --reporter spec
+
+.PHONY: test
--- test/index.js
+++ test/index.js
@@ -1,1 +1,45 @@
-// TODO: Add mocha tests
+var equal = require('assert-dir-equal');
+var Metalsmith = require('metalsmith');
+var prismic = require('..');
+var templates = require('metalsmith-templates');
+var ignore = require('metalsmith-ignore');
+
+describe('metalsmith-prismic', function(){
+    it('should retrieve content from Prismic', function(done){
+        Metalsmith('test/fixtures/basic')
+            .use(prismic({
+                "url": "http://lesbonneschoses.prismic.io/api"
+            }))
+
+            //.use (log())
+
+            // use Handlebars templating engine to insert content
+            .use(templates({
+                "engine": "handlebars",
+                "directory": "src/templates"    // templates need to be in src so they can be watched
+            }))
+
+            // do not include templating files into build
+            .use(ignore('templates/*'))
+
+            .build(function(err){
+                if (err) return done(err);
+                equal('test/fixtures/basic/expected', 'test/fixtures/basic/build');
+                done();
+            });
+    });
+});
+
+
+
+// Used for debugging purposes only
+function log() {
+    return function (files, metalsmith, done){
+        for (var file in files) {
+            if (files[file].prismic != null) {
+                console.log("%s: %s", file, JSON.stringify(files[file]))
+            }
+        }
+        done();
+    };
+}
